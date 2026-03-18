b52669b6dbe95de2dbba435b7d33ddeed1daa618
SazrBazr <sarizriek@gmail.com>
2026-03-18 02:02:42 +0200

    Upgrade https-proxy-agent to v7 for Node compatibility

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+
 const { HttpsProxyAgent } = require('https-proxy-agent'); 
+
 const app = express();
 
 app.use(cors());
